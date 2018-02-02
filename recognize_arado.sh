509b415421d46f16d1c17262f7663a1ea0cb15db
Jarno Rantala <jarno.rantala@arado.fi>
2018-02-02 15:26:52 +0200

    cleaning the new recognize script

--- recognize_arado.sh
+++ recognize_arado.sh
@@ -1,1 +1,1 @@
-python src/recognize_arado.py arado/arado_160_test/jarno/pic11.png ../models/20170512-110547/20170512-110547.pb ../models/arado_classifier.pkl --batch_size 1000
+python src/recognize_arado.py arado/arado_160_test/jarno/pic11.png --model ../models/20170512-110547/20170512-110547.pb --classifier ../models/arado_classifier.pkl
--- src/recognize_arado.py
+++ src/recognize_arado.py
@@ -42,16 +42,6 @@ def main(args):
             
             np.random.seed(seed=args.seed)
 
-            #dataset = facenet.get_dataset(args.data_dir)
-
-            #for cls in dataset:
-            #    assert (len(cls.image_paths) > 0, 'There must be at least one image for each class in the dataset')
-
-            #paths, labels = facenet.get_image_paths_and_labels(dataset)
-            
-            #print('Number of classes: %d' % len(dataset))
-            #print('Number of images: %d' % len(paths))
-
             image_path = os.path.expanduser(args.image_path)
             print('Image filename: %s' % image_path)
 
@@ -67,13 +57,6 @@ def main(args):
             
             # Run forward pass to calculate embeddings
             print('Calculating features for images')
-            nrof_images = 1
-            nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / args.batch_size))
-            emb_array = np.zeros((nrof_images, embedding_size))
-            #for i in range(nrof_batches_per_epoch):
-            #    start_index = i*args.batch_size
-            #    end_index = min((i+1)*args.batch_size, nrof_images)
-            #    paths_batch = paths[start_index:end_index]
             images = facenet.load_data([image_path], False, False, args.image_size)
             feed_dict = { images_placeholder:images, phase_train_placeholder:False }
             emb_array = sess.run(embeddings, feed_dict=feed_dict)
@@ -86,7 +69,6 @@ def main(args):
                 (model, class_names) = pickle.load(infile)
 
             print('Loaded classifier model from file "%s"' % classifier_filename_exp)
-
             predictions = model.predict_proba(emb_array)
             best_class_indices = np.argmax(predictions, axis=1)
             best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
@@ -110,30 +92,19 @@ def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_clas
             
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
-
     parser.add_argument('image_path', type=str,
         help='Path to the image to recognize.')
-    parser.add_argument('model', type=str, 
-        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
-    parser.add_argument('classifier_filename', 
+    parser.add_argument('--model', type=str,
+        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file',
+        default='../models/20170512-110547/20170512-110547.pb')
+    parser.add_argument('--classifier_filename',
         help='Classifier model file name as a pickle (.pkl) file. ' + 
-        'For training this is the output and for classification this is an input.')
-    parser.add_argument('--use_split_dataset', 
-        help='Indicates that the dataset specified by data_dir should be split into a training and test set. ' +  
-        'Otherwise a separate test set can be specified using the test_data_dir option.', action='store_true')
-    parser.add_argument('--test_data_dir', type=str,
-        help='Path to the test data directory containing aligned images used for testing.')
-    parser.add_argument('--batch_size', type=int,
-        help='Number of images to process in a batch.', default=90)
+        'For training this is the output and for classification this is an input.',
+        default='../models/arado_classifier.pkl')
     parser.add_argument('--image_size', type=int,
         help='Image size (height, width) in pixels.', default=160)
     parser.add_argument('--seed', type=int,
         help='Random seed.', default=666)
-    parser.add_argument('--min_nrof_images_per_class', type=int,
-        help='Only include classes with at least this number of images in the dataset', default=20)
-    parser.add_argument('--nrof_train_images_per_class', type=int,
-        help='Use this number of images from each class for training and the rest for testing', default=10)
-    
     return parser.parse_args(argv)
 
 if __name__ == '__main__':
