e1be76b36028dfa06be1e0a7b9e317134eb7d676
Jarno Rantala <jarno.rantala@arado.fi>
2018-02-02 16:29:55 +0200

    Fixed a couple of wrong path refs

--- recognize_arado.sh
+++ recognize_arado.sh
@@ -1,1 +1,1 @@
-python src/recognize_arado.py arado/arado_160_test/jarno/pic11.png --model ./models/20170512-110547/20170512-110547.pb --classifier ../models/arado_classifier.pkl
+python src/recognize_arado.py arado/arado_160_test/jarno/pic11.png --model ./models/20170512-110547/20170512-110547.pb --classifier ./models/arado_classifier.pkl
