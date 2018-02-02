ee7680388cca5da9c6adfbf7f92ec60d9b931748
Stefano Garusi <stefanogarusi@Stefanos-MacBook-Pro.local>
2018-02-02 16:03:35 +0200

    Changed proportion of test to train images and added markus nice picture

--- src/align/create_test_train_sets.py
+++ src/align/create_test_train_sets.py
@@ -3,39 +3,46 @@ import os
 import shutil
 
 
-arado160 = os.getcwd() + "/arado/arado_160/"
-arado160test = os.getcwd() + '/arado/arado_160_test/'
-arado160train = os.getcwd() + '/arado/arado_160_train/'
-arado160directories = next(os.walk('arado/arado_160'))[1]
+arado_160 = os.getcwd() + "/arado/arado_160/"
+arado_160_test = os.getcwd() + '/arado/arado_160_test/'
+arado_160_train = os.getcwd() + '/arado/arado_160_train/'
+arado_160_directories = next(os.walk('arado/arado_160'))[1]
 
 
 def create_files(dir):
-    filesInDir = os.listdir(arado160 + '/' + dir)
-    for i in xrange(0, len(filesInDir)/2):
-        if(not os.path.isfile(arado160test + dir + '/' + filesInDir[i])):
-            print("Copy file " + filesInDir[i] +
-                  " to dir: " + arado160test + dir)
-            shutil.copyfile(arado160 + dir + '/' +
-                            filesInDir[i], arado160test + dir + '/' + filesInDir[i])
-    for i in xrange(len(filesInDir)/2, len(filesInDir)):
-        if(not os.path.isfile(arado160train + dir + '/' + filesInDir[i])):
-            print("Copy file " + filesInDir[i] +
-                  " to dir: " + arado160train + dir)
-            shutil.copyfile(arado160 + dir + '/' +
-                            filesInDir[i], arado160train + dir + '/' + filesInDir[i])
+    files_in_dir = os.listdir(arado_160 + '/' + dir)
+    proportion_test_train = 2
+    if(len(files_in_dir) > 2):
+        proportion_test_train = 3
+
+    for i in xrange(0, len(files_in_dir)/proportion_test_train):
+        if(not os.path.isfile(arado_160_test + dir + '/' + files_in_dir[i])):
+            print("Copy file " + files_in_dir[i] +
+                  " to dir: " + arado_160_test + dir)
+            shutil.copyfile(arado_160 + dir + '/' +
+                            files_in_dir[i], arado_160_test + dir + '/' + files_in_dir[i])
+
+    for i in xrange(len(files_in_dir)/proportion_test_train, len(files_in_dir)):
+        if(not os.path.isfile(arado_160_train + dir + '/' + files_in_dir[i])):
+            print("Copy file " + files_in_dir[i] +
+                  " to dir: " + arado_160_train + dir)
+            shutil.copyfile(arado_160 + dir + '/' +
+                            files_in_dir[i], arado_160_train + dir + '/' + files_in_dir[i])
 
 
 def create_directories(dir):
-    dirToAddTest = arado160test + dir
-    dirToAddTrain = arado160train + dir
-    if not os.path.isdir(dirToAddTest):
-        print('Creating directory ' + dirToAddTest)
-        os.makedirs(dirToAddTest)
-    if not os.path.isdir(dirToAddTrain):
-        print('Creating directory ' + dirToAddTrain)
-        os.makedirs(dirToAddTrain)
+    dir_to_be_added_to_test = arado_160_test + dir
+    dir_to_be_added_to_train = arado_160_train + dir
+
+    if not os.path.isdir(dir_to_be_added_to_test):
+        print('Creating directory ' + dir_to_be_added_to_test)
+        os.makedirs(dir_to_be_added_to_test)
+
+    if not os.path.isdir(dir_to_be_added_to_train):
+        print('Creating directory ' + dir_to_be_added_to_train)
+        os.makedirs(dir_to_be_added_to_train)
 
 
-for dir in arado160directories:
+for dir in arado_160_directories:
     create_directories(dir)
     create_files(dir)
