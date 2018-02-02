86ce5efdfee9fcb26ce5ed761ba0e06e72e57175
Jarno Rantala <jarno.rantala@arado.fi>
2018-02-02 16:21:32 +0200

    Moved models folder to be part of git project

--- test_arado.sh
+++ test_arado.sh
@@ -1,1 +1,1 @@
-python src/classifier.py CLASSIFY arado/arado_160_test/ ../models/20170512-110547/20170512-110547.pb ../models/arado_classifier.pkl --batch_size 1000
+python src/classifier.py CLASSIFY arado/arado_160_test/ ./models/20170512-110547/20170512-110547.pb ../models/arado_classifier.pkl --batch_size 1000
